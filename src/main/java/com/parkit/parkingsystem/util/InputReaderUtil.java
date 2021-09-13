49754aff9d22794de64d2d2c76b2a474f380e73f
Valentin <trouillet.valentin@gmail.com>
2021-09-13 19:23:33 +0200

    Small bugs fix

--- src/main/java/com/parkit/parkingsystem/util/InputReaderUtil.java
+++ src/main/java/com/parkit/parkingsystem/util/InputReaderUtil.java
@@ -12,9 +12,8 @@ public class InputReaderUtil {
 
     public int readSelection() {
         try {
-            int input = Integer.parseInt(scan.nextLine());
-            return input;
-        }catch(Exception e){
+            return Integer.parseInt(scan.nextLine());
+        } catch (Exception e) {
             logger.error("Error while reading user input from Shell", e);
             System.out.println("Error reading input. Please enter valid number for proceeding further");
             return -1;
@@ -23,17 +22,16 @@ public class InputReaderUtil {
 
     public String readVehicleRegistrationNumber() throws Exception {
         try {
-            String vehicleRegNumber= scan.nextLine();
-            if(vehicleRegNumber == null || vehicleRegNumber.trim().length()==0) {
+            String vehicleRegNumber = scan.nextLine();
+            if (vehicleRegNumber == null || vehicleRegNumber.trim().length() == 0) {
                 throw new IllegalArgumentException("Invalid input provided");
             }
             return vehicleRegNumber;
-        }catch(Exception e){
+        } catch (Exception e) {
             logger.error("Error while reading user input from Shell", e);
             System.out.println("Error reading input. Please enter a valid string for vehicle registration number");
             throw e;
         }
     }
-
 
 }
